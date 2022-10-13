7104acc7165173a5d6dd37be5d4edf1b81444452
Lukaz Precision <30133485+lukazvprecisn@users.noreply.github.com>
2022-10-13 16:46:08 +0700

    infrastructure for testing

--- programs/test_framework/src/lib.rs
+++ programs/test_framework/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("TFXeSSo3gA2uXnZfwtHNodvAQnkMMdkZ1soXPqjXaem");
 
 #[program]
 mod tfx_test_framework {
